33e32fb27a129eb16555cd9bd8bbbc954eeb8381
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-10-10 01:28:41 +0900

    [v2.3.0+1.17] sync with v2.3.0+1.19 (#27)
    * bump version from 1.0.3 to 1.0.4+1.18 (#6)
    
    * chore: update gradles
    
    * chore: update build flow
    
    * fix: fix breaking tools
    
    * bump version from 1.0.3 to 1.0.4+1.18
    
    * docs: update README.md
    
    * bump version from 1.0.4+1.18 to 1.0.5+1.18 (#9)
    
    * chore: bump fabric versions
    
    * chore: bump Reinforced Core version from 1.1.1+1.18 to 1.1.2+1.18
    
    * chore: bump version from 1.0.4+1.18 to 1.0.5+1.18
    
    * fix: fix spaces
    
    * Add Simplified Chinese translation (#10)
    
    * bump version from 1.0.5+1.18 to 1.0.6+1.18 (#11)
    
    * chore: bump fabric versions
    
    * refactor: move dep versions
    
    * chore: bump version from 1.0.5+1.18 to 1.0.6+1.18
    
    * chore: add FUNDING.yml
    
    * bump version from 1.0.6+1.18 to 2.0.0-beta.1+1.18 (#13)
    
    * chore: bump fabric versions
    
    * chore: bump Reinforced Core version from 1.1.3+1.18 to 2.0.0-beta.1+1.18
    
    * fix: fix errors
    
    * refactor: refactor entrypoints
    
    * refactor: refactor ModStats
    
    * refactor: refactor ModBlocks
    
    * refactor: refactor ModBlockEntityType
    
    * refactor: refactor ModItems
    
    * feat: add api
    
    * feat: add entrypoint
    
    * fix: add line
    
    * refactor: require namespace
    
    * fix: fix ISSUE_TEMPLATE
    
    * fix: prevent duplicate
    
    * refactor: change logger
    
    * chore: update fabric properties link
    
    * chore: bump version from 1.0.6+1.18 to 2.0.0-beta.1+1.18
    
    * bump version from 2.0.0-beta.1+1.18 to 2.0.0+1.18 (#14)
    
    * chore: update loom and gradles
    
    * chore: bump fabric versions
    
    * chore: bump deps version
    
    * chore: update configs
    
    * chore: bump version from 2.0.0-beta.1+1.18 to 2.0.0+1.18
    
    * chore: add publish flow
    
    * bump version from 2.0.0+1.18 to 2.0.1+1.18.2 (#15)
    
    * chore: bump deps versions
    
    * style: prettier json
    
    * fix: yaml extensions
    
    * chore: bump version from 2.0.0+1.18 to 2.0.1+1.18.2
    
    * version 2.0.1 support for Minecraft 1.19-rc2 (#16)
    
    * chore: bump deps versions
    
    * fix: fix types
    
    * chore: mod version 2.0.1+1.19-rc2
    
    * docs: update README.md
    
    * bump version from 2.0.1+1.19-rc2 to 2.0.2+1.19 (#17)
    
    * chore: bump deps versions
    
    * chore: bump version from 2.0.1+1.19-rc2 to 2.0.2+1.19
    
    * bump version from 2.0.2+1.19 to 2.0.3+1.19 (#18)
    
    * chore: bump deps versions
    
    * chore: bump version from 2.0.2+1.19 to 2.0.3+1.19
    
    * bump version from 2.0.3+1.19 to 2.1.0+1.19 (#20)
    
    * chore: bump deps versions
    
    * feat: fisherman profession
    
    * chore: rename yml to yaml
    
    * docs: fix badge link
    
    * chore: bump version from 2.0.3+1.19 to 2.1.0+1.19
    
    * bump version from 2.1.0+1.19 to 2.2.0+1.19 (#21)
    
    * chore: bump deps versions
    
    * chore: add Override deco
    
    * refactor: block entity accessor
    
    * chore: add tag trigger
    
    * docs: add config section
    
    * chore: bump version from 2.1.0+1.19 to 2.2.0+1.19
    
    * bump version from 2.2.0+1.19 to 2.2.1+1.19 (#22)
    
    * chore: bump deps versions
    
    * feat: add access widener
    
    * fix: fix fishmerman poi type
    
    * chore: leave out branch trigger
    
    * chore: bump version from 2.2.0+1.19 to 2.2.1+1.19
    
    * [v2.3.0+1.19] associate mod menu (#25)
    
    * bump deps
    
    * add modmenu entrypoint
    
    * bump version
    
    * organize imports
    
    * bump version
    
    Co-authored-by: Lyaiya <hipsnafoha@outlook.com>

--- src/main/java/atonkish/reinfbarrel/util/ReinforcingMaterialSettings.java
+++ src/main/java/atonkish/reinfbarrel/util/ReinforcingMaterialSettings.java
@@ -1,6 +1,7 @@
 package atonkish.reinfbarrel.util;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
