5e23d24d50786c655c963d10e12c5b4a6a7dd9f9
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2024-12-31 10:08:23 +0900

    applied datafixer

---
+++ src/main/java/atonkish/reinfbarrel/mixin/datafixer/schema/Schema1460Mixin.java
@@ -0,0 +1,34 @@
+package atonkish.reinfbarrel.mixin.datafixer.schema;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.mojang.datafixers.DSL;
+import com.mojang.datafixers.schemas.Schema;
+import com.mojang.datafixers.types.templates.TypeTemplate;
+
+import net.minecraft.datafixer.TypeReferences;
+import net.minecraft.datafixer.schema.Schema1460;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import atonkish.reinfbarrel.ReinforcedBarrelsMod;
+
+@Mixin(Schema1460.class)
+public class Schema1460Mixin {
+    @Inject(at = @At("RETURN"), method = "registerBlockEntities", cancellable = true)
+    private void registerBlockEntities(Schema schema, CallbackInfoReturnable<Map<String, Supplier<TypeTemplate>>> cir) {
+        Map<String, Supplier<TypeTemplate>> map = cir.getReturnValue();
+
+        // TODO: materials should be able to be resolved dynamically.
+        for (String material : List.of("copper", "iron", "gold", "diamond", "netherite")) {
+            schema.register(map, String.format("%s:%s_barrel", ReinforcedBarrelsMod.MOD_ID, material), () -> {
+                return DSL.optionalFields("Items", DSL.list(TypeReferences.ITEM_STACK.in(schema)));
+            });
+        }
+    }
+}
